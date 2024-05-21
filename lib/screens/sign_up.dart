45ee1f2610943c428efd07dace2f0ebd3a7b2179
AshfaqDurjoy <137197948+AshfaqDurjoy@users.noreply.github.com>
2024-05-21 23:56:27 +0600

    emni check

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -2,4 +2,10 @@ import 'package:flutter/cupertino.dart';
 
 class SignUpView extends StatelessWidget{
   SignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
