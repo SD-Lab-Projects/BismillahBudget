30954a2c5410cd495966e8ee2f1f473378af5290
AshfaqDurjoy <137197948+AshfaqDurjoy@users.noreply.github.com>
2024-05-22 14:22:37 +0600

    Main function and Sign-Up page worked

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:bismillahbudget/screens/sign_up.dart';
 import 'package:flutter/material.dart';
 void main(){
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
@@ -14,8 +15,8 @@ class MyApp extends StatelessWidget{
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      )
+      ),
       home: MyForm(),
-    )
+    );
   }
 }
