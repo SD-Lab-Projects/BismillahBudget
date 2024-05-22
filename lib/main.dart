5cb325b7393dfd09f44d07c7b83d17a737c3c358
muhit002 <abdullah.cse.20210104107@aust.edu>
2024-05-23 00:38:54 +0600

    login screen debug solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bismillahbudget/screens/login_screen.dart';
 import 'package:bismillahbudget/screens/sign_up.dart';
 import 'package:flutter/material.dart';
 void main(){
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  SignUp_view(),
+      home:LoginView(),
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,15 @@
 import 'package:bismillahbudget/utility/Appvalidator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-//ignore_for_file:prefer_const_constructors
 
 class LoginView extends StatelessWidget {
-  LoginView({super.key});
+  LoginView({Key? key}) : super(key: key);
 
-  final GlobalKey<FormState>_formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Future<void> _submitForm() async {
     if (_formKey.currentState!.validate()) {
       ScaffoldMessenger.of(_formKey.currentContext!).showSnackBar(
-        const SnackBar(content: Text('form submitted successfully')),
+        const SnackBar(content: Text('Form submitted successfully')),
       );
     }
   }
@@ -23,133 +21,94 @@ class LoginView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF252634),
       body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
                 SizedBox(
-                height: 80.0
-              ),
-
-
-
-
-              //SizedBox
-              width: 250,
-              child: Text(
-                "Login Account",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold), // TextStyle
-              ), // Text
-            ),
-            // SizedBox
-
-            SizedBox(
-              height: 16.0,
-            ),
-            // SizedBox
-            TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(
-                  color: Color.White,
+                  height: 80.0,
+                  width: 250,
+                  child: Text(
+                    "Login Account",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-                // TextStyle
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: _buildInputDecoration("Email", Icons.email),
-                validator: appValidator.validateEmail),
-            // TextFormField
-            SizedBox(
-              height: 16.0,
-            ),
-            // SizedBox
-            TextFormField(
-              style: TextStyle(
-                color: Colors.white,
-              ),
-              //TextStyle
-              keyboardType: TextInputType.phone,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              decoration: _buildInputDecoration("Password", Icons.lock),
-              validator: appValidator.validatePassword,
-            ),
-            // TextFormField
-            SizedBox(
-              height: 40.0,
-            ),
-            // SizedBox
-            SizedBox(
-                height: 50,
-                width: double.infinity,
-                child: ElevatedButton(
+                SizedBox(height: 16.0),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: Colors.white),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: _buildInputDecoration("Email", Icons.email),
+                 // validator: appValidator.validateEmail,
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  keyboardType: TextInputType.phone,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: _buildInputDecoration("Password", Icons.lock),
+                 // validator: appValidator.validatePassword,
+                ),
+                SizedBox(height: 40.0),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFFF15900)),
+                      backgroundColor: Color(0xFFF15900),
+                    ),
                     onPressed: _submitForm,
-                    child: Text("Create",
-                        style: TextStyle(fontSize: 20)))),
-            // Text
-
-            SizedBox(
-              height: 30.0,
+                    child: Text("Create", style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpView()),
+                    );
+                  },
+                  child: Text(
+                    "Create new account",
+                    style: TextStyle(color: Color(0xFFF15900), fontSize: 20),
+                  ),
+                ),
+              ],
             ),
-            // SizedBox
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRouter(builder: (context) => signUpView()),
-                );
-              },
-              child: Text(
-                "Create new account",
-                style:
-                TextStyle(color: color(0xFFF15900), fontSize: 20),
-              ), // Text
-            )
-            // TextButton
-            ],
-          )), // Column // Form
-    ), // Padding
-    )
-    ); // SingleChildScrollView // Scaffold
+          ),
+        ),
+      ),
+    );
   }
 
-  Color
-
-  (
-
-  0xFFF15900
-
-  )
-
-  )
-
-  ,
-
-  SizedBox(
-
- color: Color(0xFFF15900), fontSize: 20, required double height),
-
   InputDecoration _buildInputDecoration(String label, IconData suffixIcon) {
+    return InputDecoration(
+      fillColor: Color(0xAA494A59),
+      labelText: label,
+      suffixIcon: Icon(suffixIcon),
+      labelStyle: TextStyle(color: Colors.white),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+      ),
+    );
+  }
+}
 
-  return
-
-  InputDecoration
-
-  (
-
-  fillColor
-
-      :
-
-  Color
-
-  (
-
-  0xAA494A59
-
-  )
+class SignUpView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement your SignUpView UI here
+    return Container();
+  }
+}
