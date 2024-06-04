5e877bb5e503bfb0d2827e53f6e294a8736c54fd
Nayemja <154550902+Nayemja@users.noreply.github.com>
2024-06-04 10:32:24 +0600

    home screen work running and there is some errors

--- lib/screens/Home_Screen.dart
+++ lib/screens/Home_Screen.dart
@@ -1,26 +1,56 @@
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+import 'login_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
   var isLogoutLoading = false;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var isLogoutLoading = false;
+
+  logout() async {
+    setState(() {
+      isLogoutLoading = true;
+    });
+    await FirebaseAuth.instance.signOut();
+    // Get.to(LoginViewPage());
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => LoginView()));
+
+    setState(() {
+      // isLogoutLoading = true;
+      isLogoutLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                logOut();
-              },
-              icon: isLogoutLoading
-                  ? CircularProgressIndicator()
-                  : Icon(Icons.exit_to_app))
-        ],
-        automaticallyImplyLeading: false,
+        body: Container(
+            width: double.infinity,
+            color: Colors.blue.shade900,
+            child: Column(
+              children: [
+                Column(
+
+                Container(
+                color: Colors.white,
+                child: Row(
+                  children: [
+
+                  ],
+                ),
+              ))],
+            )),
       ),
-      body: Text("Home"),
     );
   }
 }
