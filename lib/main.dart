4915e43ed695d367ef3186d5e58c3e4843d52699
Nayemja <154550902+Nayemja@users.noreply.github.com>
2024-06-06 01:12:24 +0600

    main class error solve

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bismillahbudget/screens/Intro.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'Widgets/auth_gate.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
