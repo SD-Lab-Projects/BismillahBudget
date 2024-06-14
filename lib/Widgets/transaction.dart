7b7cfcbb22348088e6dbb6197ed0c8fb260a14d9
Mohammad Ashfaq Hossain Bhuiyan Durjoy <ashfaqbhuiyan22@gmail.com>
2024-06-14 22:20:11 +0600

    changed transaction dart syntax

--- lib/Widgets/transaction.dart
+++ lib/Widgets/transaction.dart
@@ -1,85 +1,75 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/materlal.dart';
+import 'package:flutter/material.dart';
 
-class TranSactionsCard extends StatelessWidget {
-  const TranSactionsCard
-
-  (
-
-  {
-
-  super.key
-}
-
-@override
-Widget build(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.all(15),
-    child: Column(
-      children: [
-    Row(
-    children: [
-    Text(
-    "Recent Transactions",
-      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-    ) // Text
-    ],
-  ), // Row
-  ListView.builder(
-  shrinkWrap: true,
-  itemCount: 4,
-  physics: NeverScrollableScrollPhysics(),
-  itemBuilder: (context, index) {
-  return padding(
-  padding: const EdgeInsets.symmetric(vertical: 10),
-  child: Container(
-  decoration: BoxDecoration(
-  color: Colors.white,
-  borderRadius: BorderRadius.circular(20),
-  boxShadow: [
-  BoxShadow(
-  offset: Offset(0, 10),
-  Colors.grey.withOpacity(0.09),
-  blurRadius: 10.0,
-  spreadRadius: 4.0) // BoxShadow
-  ]), // BoxDecoration
-  child: ListTile(
-  title: Row(
-  Children: [
-  Expanded(child: Text("Car Rent Oct 2024"),
-  ]
-  Text(
-  "৳ 8000",
-  style:TextStyle(color: Colors.green),
-  ) // Text
-  ],
-  ),//Row
-  subtitle:Column(
-  mainAxisAlignment: MainAxisAlignment.start,
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children:
-  Row(children: [
-  Text("Balance", style: TextStyle(color: Colors.grey, fontSize: 13)),
-  Spacer(),
-  Text("৳ 525", style: TextStyle(color:Colors.grey, fontSize: 13))
-  ],), // Row
-
-  Text("25 oct 4:51 PM", style: TextStyle(color: Color.grey),)
-  ])//Row
-
-  ]), // Column
-  );//listTile
-  })//ListView.builder
-
+class TransactionsCard extends StatelessWidget {
+  const TransactionsCard({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  // TODO: implement build
-  throw UnimplementedError();
-  }]
-  ,
-  )
-  , // column
-  )
-  , //padding
-}}
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                "Recent Transactions",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: 4,
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        color: Colors.grey.withOpacity(0.09),
+                        blurRadius: 10.0,
+                        spreadRadius: 4.0,
+                      ),
+                    ],
+                  ),
+                  child: ListTile(
+                    title: Row(
+                      children: [
+                        Expanded(child: Text("Car Rent Oct 2024")),
+                        Text(
+                          "৳ 8000",
+                          style: TextStyle(color: Colors.green),
+                        ),
+                      ],
+                    ),
+                    subtitle: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text("Balance", style: TextStyle(color: Colors.grey, fontSize: 13)),
+                            Spacer(),
+                            Text("৳ 525", style: TextStyle(color: Colors.grey, fontSize: 13)),
+                          ],
+                        ),
+                        Text("25 Oct 4:51 PM", style: TextStyle(color: Colors.grey)),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/utility/icons_list.dart
+++ lib/utility/icons_list.dart
@@ -1,60 +1,51 @@
-import 'package:font_awsome_flutter/awsome_flutter.dart';
-  class AppIcons{
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+class AppIcons {
   final List<Map<String, dynamic>> homeExpensesCategories = [
-  {
-  "name": "Gas Filling",
-  "icon": FontAwesomeIcons.gasPump,
-},
-{
-"name": "Grocery",
-"icon": FontAwesomeIcons. shoppingCart,
-},
-{
-"name": "Milk",
-"icon": FontAwesomeIcons.mugHot,
-},
-
-{
-
-"name": "Internet",
-"icon": FontAwesomeIcons.wifi,
-},
-
-
-  {
-
-  "name": "Electricity",
-  "icon":FontAwesomeIcons.current,
+    {
+      "name": "Gas Filling",
+      "icon": FontAwesomeIcons.gasPump,
+    },
+    {
+      "name": "Grocery",
+      "icon": FontAwesomeIcons.shoppingCart,
+    },
+    {
+      "name": "Milk",
+      "icon": FontAwesomeIcons.mugHot,
+    },
+    {
+      "name": "Internet",
+      "icon": FontAwesomeIcons.wifi,
+    },
+    {
+      "name": "Electricity",
+      "icon": FontAwesomeIcons.current,
+    },
+    {
+      "name": "Transportation",
+      "icon": FontAwesomeIcons.bus,
+    },
+    {
+      "name": "Clothing",
+      "icon": FontAwesomeIcons.tshirt,
+    },
+    {
+      "name": "Insurance",
+      "icon": FontAwesomeIcons.shieldAlt,
+    },
+    {
+      "name": "Education",
+      "icon": FontAwesomeIcons.graduationCap,
+    },
+    {
+      "name": "Water",
+      "icon": FontAwesomeIcons.water,
+    },
+    {
+      "name": "Others",
+      "icon": FontAwesomeIcons.cartPlus,
+    },
+  ];
 }
-{
-"name": "Transportation",
-"icon": FontAwesomeIcons.bus,
-},
-{
-
-"name": "Clothing",
-"icon": FontAwesomeIcons.tshirt,
-
-
-
-},
-{
-"name": "Insurance",
-"icon": FontAwesomeIcons.shieldAlt,
-},
-{
-
-"name": "Education",
-"icon": FontAwesomeIcons.graduationCap,
-},
-{
-
-"name": "water",
-"icon": FontAwesomeIcons.water,
-},
-{
-"name": "others",
-"icon": FontAwesomeIcons.cartPlus,
-},
-  }
