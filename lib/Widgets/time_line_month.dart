65787300b0b06c9d01d9d9870fc58b6919b86cf7
muhit002 <abdullah.cse.20210104107@aust.edu>
2024-07-03 02:06:33 +0600

    time_line_month.dart
    time_line_month has some error.i am working on that

---
+++ lib/Widgets/time_line_month.dart
@@ -0,0 +1,28 @@
+import 'package:budgettracker/widgets/time_iine_month.dart';
+import 'package:flutter/material.dart';
+
+class TransactionScreen extends StatelessWidget {
+  const TransactionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Expansive"), // Use 'const' with the constructor to improve pe
+        ), // AppBar
+        body: Column(  //Use 'const' with the constructor to improve performance.dTry
+            children: [  // Use 'const' literals as arguments to constructors of '@immut
+            TimeLineMonth(), // Use 'const' with the constructor to improve performan
+    onChanged:(String?value) {},
+    ),//TimeLineMonth
+    CaregoryList(
+    onChanged:(String?value) {},
+    ),//CategoryList
+    ],
+    ), // Column
+    ); // Scaffold
+    }
+}
+
+
+
